d080c1500e3613272371597cc1468d37bac5e2d0
Sergey Glazov <yaglazov@gmail.com>
2018-11-02 16:47:57 +0300

    favicon and orders

--- app/resources/orders/index.php
+++ app/resources/orders/index.php
@@ -5,30 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Сайт «Броникс»: список заявок на вызов агента</title>
 
-    <link sizes="57x57"   href="assets/images/i/apple-icon-57x57.png"   rel="apple-touch-icon" />
-    <link sizes="60x60"   href="assets/images/i/apple-icon-60x60.png"   rel="apple-touch-icon" />
-    <link sizes="72x72"   href="assets/images/i/apple-icon-72x72.png"   rel="apple-touch-icon" />
-    <link sizes="76x76"   href="assets/images/i/apple-icon-76x76.png"   rel="apple-touch-icon" />
-    <link sizes="114x114" href="assets/images/i/apple-icon-114x114.png" rel="apple-touch-icon" />
-    <link sizes="120x120" href="assets/images/i/apple-icon-120x120.png" rel="apple-touch-icon" />
-    <link sizes="144x144" href="assets/images/i/apple-icon-144x144.png" rel="apple-touch-icon" />
-    <link sizes="152x152" href="assets/images/i/apple-icon-152x152.png" rel="apple-touch-icon" />
-    <link sizes="180x180" href="assets/images/i/apple-icon-180x180.png" rel="apple-touch-icon" />
-    <link href="assets/images/i/apple-icon-152x152.png" rel="apple-touch-icon" />
+    <link sizes="57x57"   href="../assets/images/i/apple-icon-57x57.png"   rel="apple-touch-icon" />
+    <link sizes="60x60"   href="../assets/images/i/apple-icon-60x60.png"   rel="apple-touch-icon" />
+    <link sizes="72x72"   href="../assets/images/i/apple-icon-72x72.png"   rel="apple-touch-icon" />
+    <link sizes="76x76"   href="../assets/images/i/apple-icon-76x76.png"   rel="apple-touch-icon" />
+    <link sizes="114x114" href="../assets/images/i/apple-icon-114x114.png" rel="apple-touch-icon" />
+    <link sizes="120x120" href="../assets/images/i/apple-icon-120x120.png" rel="apple-touch-icon" />
+    <link sizes="144x144" href="../assets/images/i/apple-icon-144x144.png" rel="apple-touch-icon" />
+    <link sizes="152x152" href="../assets/images/i/apple-icon-152x152.png" rel="apple-touch-icon" />
+    <link sizes="180x180" href="../assets/images/i/apple-icon-180x180.png" rel="apple-touch-icon" />
+    <link href="../assets/images/i/apple-icon-152x152.png" rel="apple-touch-icon" />
 
-    <link sizes="192x192" href="assets/images/i/favicon-192x192.png" rel="icon" type="image/png" />
-    <link sizes="160x160" href="assets/images/i/favicon-160x160.png" rel="icon" type="image/png" />
-    <link sizes="96x96"   href="assets/images/i/favicon-96x96.png"   rel="icon" type="image/png" />
-    <link sizes="32x32"   href="assets/images/i/favicon-32x32.png"   rel="icon" type="image/png" />
-    <link sizes="16x16"   href="assets/images/i/favicon-16x16.png"   rel="icon" type="image/png" />
+    <link sizes="192x192" href="../assets/images/i/favicon-192x192.png" rel="icon" type="image/png" />
+    <link sizes="160x160" href="../assets/images/i/favicon-160x160.png" rel="icon" type="image/png" />
+    <link sizes="96x96"   href="../assets/images/i/favicon-96x96.png"   rel="icon" type="image/png" />
+    <link sizes="32x32"   href="../assets/images/i/favicon-32x32.png"   rel="icon" type="image/png" />
+    <link sizes="16x16"   href="../assets/images/i/favicon-16x16.png"   rel="icon" type="image/png" />
 
-    <link rel="shortcut icon" href="assets/images/i/favicon.ico" type="image/x-icon" />
-    <link rel="icon" href="assets/images/i/favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="../assets/images/i/favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="../assets/images/i/favicon.ico" type="image/x-icon" />
 
-    <meta name="msapplication-square70x70logo" content="assets/images/i/smalltile.png" />
-    <meta name="msapplication-square150x150logo" content="assets/images/i/mediumtile.png" />
-    <meta name="msapplication-wide310x150logo" content="assets/images/i/widetile.png" />
-    <meta name="msapplication-square310x310logo" content="assets/images/i/largetile.png" />
+    <meta name="msapplication-square70x70logo" content="../assets/images/i/smalltile.png" />
+    <meta name="msapplication-square150x150logo" content="../assets/images/i/mediumtile.png" />
+    <meta name="msapplication-wide310x150logo" content="../assets/images/i/widetile.png" />
+    <meta name="msapplication-square310x310logo" content="../assets/images/i/largetile.png" />
 
     <meta name="theme-color" content="#1D1D1D" />
 
@@ -40,11 +40,11 @@
     <meta name="msapplication-TileColor" content="#1D1D1D" />
     <meta name="msapplication-starturl" content="" />
     <meta name="msapplication-tap-highlight" content="no" />
-    <meta name="msapplication-TileImage" content="assets/images/i/ms-icon-144x144.png" />
+    <meta name="msapplication-TileImage" content="../assets/images/i/ms-icon-144x144.png" />
 
 
-    <link rel="manifest" href="/manifest.json"/>
-    <meta name="msapplication-config" content="/browserconfig.xml" />
+    <link rel="manifest" href="../manifest.json"/>
+    <meta name="msapplication-config" content="../browserconfig.xml" />
     <link href='https://fonts.googleapis.com/css?family=PT+Sans:400,400italic,700,700italic&subset=latin,cyrillic-ext' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
@@ -86,7 +86,7 @@
           </thead>
           <tbody>
             <?
-              $fileorder = '_dataforms.txt';
+              $fileorder = '_orders.txt';
               if ( file_exists($fileorder) ) {
                 include("_orders.txt");
               } else {
